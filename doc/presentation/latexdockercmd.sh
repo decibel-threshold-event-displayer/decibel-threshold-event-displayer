69fd7195fafefd5d5ffc7379d7c04c6365d9f0b3
Dominic Gernert <dominic.gernert@students.bfh.ch>
2024-10-05 19:55:35 +0200

    feat: added a clear function for latex build files [no ticket]

--- doc/presentation/latexdockercmd.sh
+++ doc/presentation/latexdockercmd.sh
@@ -1,3 +1,17 @@
 #!/bin/sh
 IMAGE=blang/latex:ubuntu
-exec docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "$PWD":/data "$IMAGE" "$@"
+
+if [ "$#" = "1" ] && [ "$1" = "clear" ]; then
+
+  for file in $(find presentation.*)
+  do
+    if [ $file != "presentation.tex" ]; then
+      rm $file
+    fi
+  done
+
+else
+
+  exec docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "$PWD":/data "$IMAGE" "$@"
+
+fi
--- doc/report/latexdockercmd.sh
+++ doc/report/latexdockercmd.sh
@@ -1,3 +1,17 @@
 #!/bin/sh
 IMAGE=blang/latex:ubuntu
-exec docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "$PWD":/data "$IMAGE" "$@"
+
+if [ "$#" = "1" ] && [ "$1" = "clear" ]; then
+
+  for file in $(find report.*)
+  do
+    if [ $file != "report.tex" ]; then
+      rm $file
+    fi
+  done
+
+else
+
+  exec docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "$PWD":/data "$IMAGE" "$@"
+
+fi
