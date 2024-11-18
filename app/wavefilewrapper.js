828d55d7f2d31f0eb656f7cd99ce8c85a3038924
Dominic Gernert <dominic.gernert@students.bfh.ch>
2024-11-18 20:34:02 +0100

    feat: added 5min test file [#10]

--- app/wavefilewrapper.js
+++ app/wavefilewrapper.js
@@ -29,12 +29,13 @@ const DATA_IDENTIFIER = 0x64617461; // 'data'
 const WAVE_FILE_FORMAT = 0x57415645; // 'WAVE'
 
 export class WaveFileWrapper {
-    constructor(file) {
-        this.nbrOfChannels = 0;
-        this.nbrOfSamples = 0;
-        this.samplesPerSecond = 0;
-        this.bytesPerSample = 0;
+    samples = [];
+    nbrOfChannels = 0;
+    nbrOfSamples = 0;
+    samplesPerSecond = 0;
+    bytesPerSample = 0;
 
+    constructor(file) {
         if (file instanceof ArrayBuffer) {
             this.filename = "Unknown File";
             this.parseFile(file);
@@ -46,7 +47,7 @@ export class WaveFileWrapper {
         }
     }
 
-    readAndParse = function (file) {
+    readAndParse(file) {
         const reader = new FileReader()
 
         reader.addEventListener("loadend", () => {
@@ -64,7 +65,7 @@ export class WaveFileWrapper {
         }
     }
 
-    parseFile = function (arrayBuffer) {
+    parseFile(arrayBuffer) {
         // https://en.wikipedia.org/wiki/WAV#WAV_file_header
 
         let riffView = new DataView(arrayBuffer, CHUNK_OFFSET_RIFF, CHUNK_LENGTH_RIFF);
@@ -81,65 +82,64 @@ export class WaveFileWrapper {
         this.parseAndVerifyDataChunk(dataView);
     };
 
-    getDataOffset = function (dataView) {
+    getDataOffset(dataView) {
         // https://www.recordingblogs.com/wiki/list-chunk-of-a-wave-file
         let identifier = dataView.getInt32(LIST_OFFSET_IDENTIFIER);
 
-        if (identifier == LIST_IDENTIFIER)
+        if (identifier === LIST_IDENTIFIER)
             return dataView.getInt32(LIST_OFFSET_SIZE, true) + 8;
         else
             return 0;
     }
 
-    parseAndVerifyRiffChunk = function (dataView) {
+    parseAndVerifyRiffChunk(dataView) {
         // check identifiert
         var identifier = dataView.getInt32(RIFF_OFFSET_IDENTIFIER);
 
-        if (identifier != RIFF_IDENTIFIER)
+        if (identifier !== RIFF_IDENTIFIER)
             throw new InvalidRiffChunkError("File identifiert is not 'RIFF'");
 
         // check file format
         var fileFormatId = dataView.getInt32(RIFF_OFFSET_FILE_FORMAT_ID);
 
-        if (fileFormatId != WAVE_FILE_FORMAT)
+        if (fileFormatId !== WAVE_FILE_FORMAT)
             throw new InvalidRiffChunkError("RIFF format is not 'WAVE'");
     }
 
-    parseAndVerifyFormatChunk = function (dataView) {
+    parseAndVerifyFormatChunk(dataView) {
         // check identifiert
         let identifier = dataView.getInt32(FORMAT_OFFSET_IDENTIFIER);
 
-        if (identifier != FORMAT_IDENTIFIER)
+        if (identifier !== FORMAT_IDENTIFIER)
             throw new InvalidFormatChunkError("Format chunk identifiert is not 'fmt '");
 
         let audioFormat = dataView.getInt16(FORMAT_OFFSET_AUDIO_FORMAT, true);
 
         // must be PCM for the moment
-        if (audioFormat != 1)
+        if (audioFormat !== 1)
             throw new InvalidFormatChunkError("Only PCM Format is supported");
 
         this.nbrOfChannels = dataView.getInt16(FORMAT_OFFSET_NBR_OF_CHANNELS, true);
         this.samplesPerSecond = dataView.getInt32(FORMAT_OFFSET_SAMPLES_PER_SECOND, true);
         let bitsPerSample = dataView.getInt16(FORMAT_OFFSET_BITS_PER_SAMPLE, true);
 
-        if (bitsPerSample % 8 != 0)
+        if (bitsPerSample % 8 !== 0)
             throw new InvalidFormatChunkError(`Bits per sample are ${bitsPerSample}, but have to be a multiple of 8`);
 
         this.bytesPerSample = bitsPerSample / 8;
     }
 
-    parseAndVerifyDataChunk = function (dataView) {
+    parseAndVerifyDataChunk(dataView) {
         // check identifier
         let identifier = dataView.getInt32(DATA_OFFSET_IDENTIFIER);
 
-        if (identifier != DATA_IDENTIFIER)
+        if (identifier !== DATA_IDENTIFIER)
             throw new InvalidDataChunkError("Data chunk identifiert is not 'data'");
 
         let dataSize = dataView.getInt32(DATA_OFFSET_SIZE, true);
 
         let nbrOfSamples = dataSize / (this.bytesPerSample * this.nbrOfChannels);
         let offset = DATA_OFFSET_SAMPLES;
-        this.samples = [];
 
         for (let i = 0; i < nbrOfSamples; ++i) {
             this.samples[i] = [];
@@ -158,12 +158,70 @@ export class WaveFileWrapper {
         }
     }
 
-    toString = function() {
+    toString() {
         return `Filename: ${this.filename}\n
                 Number of channels: ${this.nbrOfChannels}\n
                 Number of samples ${this.nbrOfSamples}\n
                 Samples per second ${this.samplesPerSecond}\n
                 Bytes per samples ${this.bytesPerSample}\n`;
+    }
+
+    async sampleToDecibel(sample) {
+        // Validate arguments
+        if (!Number.isInteger(sample) || sample < 0 ) {
+            throw new Error("Invalid argument, sample must be an integer equal or greater than 0!");
+        }
+
+        return 20 * Math.log10(sample)
+    }
+
+    /**
+     * Calculate the root-mean-square value for all samples in a frame
+     *
+     * @param framesPerSecond
+     * @returns {*[]}
+     */
+    mapRMSFrames(framesPerSecond = 0.3) {
+        // Validate arguments
+        if (!Number.isFinite(framesPerSecond) || framesPerSecond <= 0 ) {
+            throw new Error("Invalid argument, chungDurationMilliseconds must be an integer greater than 0!");
+        }
+
+        // Check that we have at least some samples
+        if (!this.samples || this.samples.length === 0) {
+            throw new Error("No samples available to calculate RMS");
+        }
+
+        const frameSize =  Math.floor(framesPerSecond * this.samplesPerSecond)
+
+        const rmsFrames = [];
+        // We round up, otherwise some samples might get cut off or no samples get processed at all,
+        // if the frameSize is larger than the number of samples
+        const totalFrames = Math.ceil(this.samples.length / frameSize);
+
+        for (let frameIndex = 0; frameIndex < totalFrames; frameIndex++) {
+            const startSample = frameIndex * frameSize;
+            // This handles the edge case for the last frame, where we might have fewer samples as the full frame
+            const endSample = Math.min( startSample + frameSize, this.samples.length);
+            let frameSum = 0;
+
+            for (let i = startSample; i < endSample; i++) {
+                for (let channel = 0; channel < this.nbrOfChannels; channel++) {
+                    const sample = this.samples[i][channel];
+                    frameSum += sample ** 2;
+                }
+            }
+
+            // This handles the edge case for the last frame, where we might have fewer samples as the full frame
+            const actualFrameSize = endSample - startSample;
+            const meanSquare = frameSum / (actualFrameSize * this.nbrOfChannels);
+            rmsFrames.push({
+                millisecond: startSample / (this.samplesPerSecond * 1000),
+                value: this.sampleToDecibel(Math.sqrt(meanSquare)),
+            });
+        }
+
+        return rmsFrames;
     }
 }
 
