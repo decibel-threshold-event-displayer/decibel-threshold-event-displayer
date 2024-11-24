e358cfd4858d25ddf9d6828d3f5f56f2d2ca6009
Darius Andrin Degel <darius.degel@students.bfh.ch>
2024-11-24 12:22:09 +0100

    feat: re-implement engine [#103]

--- app/js/components/home/home.js
+++ app/js/components/home/home.js
@@ -1,6 +1,11 @@
+import { engine } from "../../engine.js";
 import { Component } from "../component.js";
 
 export class Home extends Component {
+  #status;
+  #embed;
+  #renderButton;
+
   constructor() {
     super("Decibel Threshold Event Displayer", "", "center");
     this.#init();
@@ -8,5 +13,18 @@ export class Home extends Component {
 
   async #init() {
     await this._load("home/home.html");
+    this.#status = this._select("#status");
+    this.#embed = this._select("#embed");
+    this.#renderButton = this._select("#render");
+    this.#renderButton.onclick = () => this.#render();
+  }
+
+  async #render() {
+    const res = await engine.generate((status) => this.#onStatusUpdate(status));
+    this.#embed.src = res;
+  }
+
+  #onStatusUpdate(status) {
+    this.#status.innerHTML = `STATUS: ${status}`;
   }
 }
---
+++ app/js/engine.js
@@ -0,0 +1,79 @@
+import { ENGINE_GENERATE_STATUS, ENGINE_STATUS } from "./enum.js";
+import { store } from "./store.js";
+
+class Engine {
+  #engine;
+  #readyPromise;
+
+  constructor() {
+    this.#init();
+  }
+
+  async #init() {
+    store.setEngineStatus(ENGINE_STATUS.LOADING);
+    console.log("INITING");
+    try {
+      const dependencies = await fetch(
+        "js/swiftlatex/dependencies.local.json"
+      ).then((response) => response.json());
+
+      await Promise.all(dependencies.map(async (link) => fetch(link))).then(
+        (responses) => Promise.all(responses.map((res) => res.text()))
+      );
+
+      const engine = new PdfTeXEngine();
+      await engine.loadEngine();
+      await engine.latexWorker.postMessage({
+        cmd: "settexliveurl",
+        url: "/js/swiftlatex/dependencies/",
+      });
+
+      this.#engine = engine;
+
+      store.setEngineStatus(ENGINE_STATUS.READY);
+    } catch (e) {
+      store.setEngineStatus(ENGINE_STATUS.ERROR);
+      throw e;
+    }
+  }
+
+  async #getEngine() {
+    if (this.#engine) return this.#engine;
+    else if (!this.#readyPromise)
+      this.#readyPromise = this.#init()
+        .then(() => this.#engine)
+        .finally(() => (this.#readyPromise = null));
+    return this.#readyPromise;
+  }
+
+  async generate(onStatusUpdate = (status) => {}) {
+    try {
+      onStatusUpdate(ENGINE_GENERATE_STATUS.INITIALIZATION);
+      const engine = await this.#getEngine();
+
+      onStatusUpdate(ENGINE_GENERATE_STATUS.READYING_FILE);
+      const template = await fetch("js/swiftlatex/template.tex").then(
+        (response) => response.text()
+      );
+      engine.writeMemFSFile("main.tex", template);
+      engine.setEngineMainFile("main.tex");
+      onStatusUpdate(ENGINE_GENERATE_STATUS.GENERATING);
+      const result = await engine.compileLaTeX();
+      console.log(result.log);
+
+      if (result.status === 0) {
+        const pdfblob = new Blob([result.pdf], { type: "application/pdf" });
+        const objectURL = URL.createObjectURL(pdfblob);
+        console.log(objectURL);
+        onStatusUpdate(ENGINE_GENERATE_STATUS.DONE);
+        return objectURL;
+      } else throw new Error("Something went wrong generating the PDF.");
+    } catch (e) {
+      onStatusUpdate(ENGINE_GENERATE_STATUS.ERROR);
+      console.error(e);
+      throw e;
+    }
+  }
+}
+
+export const engine = new Engine();
